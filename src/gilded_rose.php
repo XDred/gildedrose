133205e2862e7102f1327a6d92601d9ea5b76c73
XDred <hikita.beer@gmail.com>
2018-05-28 13:43:49 +0400

    fix

--- src/gilded_rose.php
+++ src/gilded_rose.php
@@ -1,110 +1,135 @@
 <?php
 
 /*
-*   Нужно было добавить возможность легко добавлять новые категории товаров со своими свойствами
-*   
-*   для этого я добавил 2 массива - по умолчанию и настройки
-*/
+ *   Нужно было добавить возможность легко добавлять новые категории товаров со своими свойствами
+ *
+ *   для этого я добавил 2 массива - по умолчанию и настройки
+ */
 
-
-class GildedRose {
+class GildedRose
+{
 
     private $items;
 
     private $default = array( // массив по умолчанию - применяется если не предмет не найден в массиве настроек
-        'sellin' => -1,  // увеличение параметра sell_in за шаг
+        'sellin'  => -1, // увеличение параметра sell_in за шаг
         'quality' => -1, // увеличение параметра quality за шаг
         'expired' => -2, // увеличение параметра quality за шаг, если продукт просрочен
     );
 
     private $settings = array( // массив настроек - описывает параметры предметов
         'Sulfuras, Hand of Ragnaros' => array( // сульфурас легендарный - у него ничего не меняется
-            'sellin' => 0,
+            'sellin'  => 0,
             'quality' => 0,
             'expired' => 0,
         ),
-        'Aged Brie' => array( // бри прибавляет качества со временем
-            'sellin' => -1,
+        'Aged Brie'                  => array( // бри прибавляет качества со временем
+            'sellin'  => -1,
             'quality' => 1,
             'expired' => 2,
         ),
-        'Conjured' => array( // призванные товары теряют качество в 2 раза сильнее обычных
-            'sellin' => -1,
+        'Conjured'                   => array( // призванные товары теряют качество в 2 раза сильнее обычных
+            'sellin'  => -1,
             'quality' => -2,
             'expired' => -4,
         ),
-        'Backstage passes' => array(
-            'sellin' => -1, 
+        'Backstage passes'           => array(
+            'sellin'  => -1,
             'quality' => 1,
             'expired' => '=0', // установить quality сразу в 0 если продукт просрочен
-            'days' => array( // особый параметр, отвечает за изменение качества в зависимости от оставшихся дней
+            'days'    => array( // особый параметр, отвечает за изменение качества в зависимости от оставшихся дней
                 10 => 2, // если меньше 10 то +2
-                5 => 3, // если меньше 5 то +3
+                5  => 3, // если меньше 5 то +3
             ), // впринципе можно было объеденить с expired но я подумал что так будет лучше
         ),
     );
 
-    function __construct($items) {
+    public function __construct($items)
+    {
         $this->items = $items;
     }
 
-    function update_quality() {
+    public function update_quality()
+    {
         foreach ($this->items as $item) {
-            $settings = array_keys($this->settings); // берём ключи массива настроек
+            $settings    = array_keys($this->settings); // берём ключи массива настроек
             $cur_setting = $this->default; // записываем настройку по умолчанию в качестве текущей
-            foreach($settings as $setting){
-                if(strpos($item->name,$setting) !== false){ // если название предмета содержит ключ из массива
+            foreach ($settings as $setting) {
+                if (strpos($item->name, $setting) !== false) { // если название предмета содержит ключ из массива
                     $cur_setting = $this->settings[$setting]; // то запоминаем эту настройку в качестве текущей
                     break; // и выходим из цикла
                 }
             }
 
             $item->sell_in = $item->sell_in + $cur_setting['sellin']; // уменьшаем количество оставшихся дней на столько сколько указано в настройках
-            
-            if($item->sell_in < 0){ // если продукт просрочен
-                if($cur_setting['expired'] === '=0'){ // если в настройках указано что просрочку сразу устанавливаем в 0
+
+            if ($item->sell_in < 0) { // если продукт просрочен
+                if ($cur_setting['expired'] === '=0') { // если в настройках указано что просрочку сразу устанавливаем в 0
                     $item->quality = 0; // ставим 0
-                }else{
-                    if(($item->quality+$cur_setting['expired'] >=0) && ($item->quality+$cur_setting['expired'] <=50)){ // если итоговый параметр не выбивается за границы качества
-                        $item->quality = $item->quality + $cur_setting['expired']; // добавляем сколько нужно
+                } else {
+                    if ($cur_setting['expired'] != 0) { // если изменение качества не равно нулю
+                        $item->quality = $item->quality + $cur_setting['expired']; // добавляем качество
+                        if ($item->quality < 0) {
+                            $item->quality = 0;
+                        }
+                        if ($item->quality > 50) {
+                            $item->quality = 50;
+                        }
                     }
                 }
-            }else{ // соответственно, если продукт не просрочен
-                if(isset($cur_setting['days'])){ // проверяем, есть ли у него особый параметр 
-                    $days = array_keys($cur_setting['days']); //берём дни
+            } else { // соответственно, если продукт не просрочен
+                if (isset($cur_setting['days'])) { // проверяем, есть ли у него особый параметр
+                    $days        = array_keys($cur_setting['days']); //берём дни
                     $cur_quality = $cur_setting['quality']; // запоминаем текущую настройку без учёта дней как настройку по умолчанию
                     rsort($days); // сортируем, чтобы всегда было правильно
-                    foreach ($days as $day){
-                        if($item->sell_in < $day) $cur_quality = $cur_setting['days'][$day]; // и ищем нужную настройку
-                    }                        
-                    if(($item->quality+$cur_quality >=0) && ($item->quality+$cur_quality <=50)){ // если итоговый параметр не выбивается за границы качества 
-                        $item->quality = $item->quality + $cur_quality; // добавляем сколько нужно
-                    }        
-                }else{ // если нет особого параметра
-                    if(($item->quality+$cur_setting['quality'] >=0) && ($item->quality+$cur_setting['quality'] <=50)){ // если итоговый параметр не выбивается за границы качества 
-                        $item->quality = $item->quality + $cur_setting['quality']; // добавляем сколько нужно
+                    foreach ($days as $day) {
+                        if ($item->sell_in < $day) {
+                            $cur_quality = $cur_setting['days'][$day];
+                        }
+                        // и ищем нужную настройку
+                    }
+                    if ($cur_quality != 0) { // если изменение качества не равно нулю
+                        $item->quality = $item->quality + $cur_quality; // добавляем качество
+                        if ($item->quality < 0) {
+                            $item->quality = 0;
+                        }
+                        if ($item->quality > 50) {
+                            $item->quality = 50;
+                        }
+                    }
+                } else { // если нет особого параметра
+                    if ($cur_setting['quality'] != 0) { // если изменение качества не равно нулю
+                        $item->quality = $item->quality + $cur_setting['quality']; // добавляем качество
+                        if ($item->quality < 0) {
+                            $item->quality = 0;
+                        }
+                        if ($item->quality > 50) {
+                            $item->quality = 50;
+                        }
                     }
                 }
-            }   // готово, мы великолепны         
+            } // готово, мы великолепны
         }
     }
 }
 
-class Item {
+class Item
+{
 
     public $name;
     public $sell_in;
     public $quality;
 
-    function __construct($name, $sell_in, $quality) {
-        $this->name = $name;
+    public function __construct($name, $sell_in, $quality)
+    {
+        $this->name    = $name;
         $this->sell_in = $sell_in;
         $this->quality = $quality;
     }
 
-    public function __toString() {
+    public function __toString()
+    {
         return "{$this->name}, {$this->sell_in}, {$this->quality}";
     }
 
 }
-
